30488ed3947461ccbed6b4e570ece81183f49e69
Hippolyte Moranne <hippolyte.moranne@etu.minesparis.psl.eu>
2024-01-17 11:47:08 +0100

    amelioration de benchmark

--- benchmark.py
+++ benchmark.py
@@ -22,19 +22,17 @@ def mel_cs(y, y_predicted, sr) :
     y_predicted =y_predicted.astype(np.float64)
     y_predicted = y_predicted/32768
     sample_rate = sr
-    win_length = 240
-    n_fft = 512#int(np.floor(np.log2(len(y)))) = 9 dans notre cas#
-    hop_length = 50
+    win_length = 512
+    n_fft = 1024#int(np.floor(np.log2(len(y)))) = 9 dans notre cas#
+    hop_length = 256
     y_spec = melspectrogram(y=y, sr=sample_rate, S= None, n_fft=n_fft, hop_length= hop_length, win_length=win_length, window='hann', center=True, pad_mode='constant', power=2.0, n_mels = 64)
     y_pred_spec = melspectrogram(y=y_predicted, sr=sample_rate, S= None, n_fft=n_fft, hop_length=hop_length, win_length=win_length, window='hann', center=True, pad_mode='constant', power=2.0, n_mels = 64)
     diff = np.linalg.norm(y_spec-y_pred_spec, ord='fro')
-    #plt.figure(figsize=(40, 40), dpi=80)
-    
+    #plt.figure(figsize=(9, 4), dpi=80)
+    #specshow(y_spec-y_pred_spec, hop_length = hop_length, x_axis='time', y_axis='log')
     #fig, [ax1, ax2] = plt.subplots(nrows = 1, ncols=2)
     #img_1 = specshow(y_spec, hop_length = hop_length, x_axis='time', y_axis='log', ax=ax1)
-    ##ax.set(title='Now with labeled axes!')
     #fig.colorbar(img_1, ax=ax1, format="%+2.f dB")
-#
     #img_2 = specshow(y_pred_spec, hop_length = hop_length, x_axis='time', y_axis='log', ax=ax2)
     #fig.colorbar(img_2, ax=ax2, format="%+2.f dB")
     #plt.show()
